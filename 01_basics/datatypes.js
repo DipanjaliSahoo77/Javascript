d6c0af76c8469d5040273a5317fa94056c995ea1
DIPANJALI SAHOO <139108012+DipanjaliSahoo77@users.noreply.github.com>
2026-01-17 05:27:12 +0000

    datatypes in javascript

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"//treat all js code as a new vesrion
+// alert(3+6)//in node.js we can not use like this but in browser we can use this.
+console.log(3+7)
+console.log("dipanjali")
+let age=22
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint
+//string=>"" or''
+//boolean=> true/false
+//null=>standloane value and it is foe empty value
+//undefined=>value is not assigned 
+//symbol=>unique
+console.log(typeof null)//it will return an object so null is a object
+console.log(typeof undefined)//it will retun undefined

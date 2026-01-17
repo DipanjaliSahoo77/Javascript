b3264c6e922c673b2ef4de1bf2ac731ac3aeed59
DIPANJALI SAHOO <139108012+DipanjaliSahoo77@users.noreply.github.com>
2026-01-17 05:44:13 +0000

    conversion of datatypes

---
+++ 01_basics/convesrionoperation.js
@@ -0,0 +1,20 @@
+let score="dipa"
+console.log(typeof score);
+let valueInNumber=Number(score)//it will convert string to number
+console.log (typeof valueInNumber)
+console.log(valueInNumber) 
+//"12"=>33
+//"123dipa"=> NaN
+//true=>1; false=>0
+//undefined=>NaN
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)//1 convert to true 
+console.log(booleanIsLoggedIn)
+//1=>true, 0=>false
+//""=>false, "dipa"=>true
+
+let someNumber=23
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

9b6c5863c278771526a2c70d70b70c8dc6ce2305
DIPANJALI SAHOO <139108012+DipanjaliSahoo77@users.noreply.github.com>
2026-01-17 07:17:05 +0000

    comparision in js

---
+++ 01_basics/comparision.js
@@ -0,0 +1,10 @@
+console.log("2">1)
+console.log(null>0)
+console.log(null==0)
+console.log(null >=0)
+/*
+here null converted to 0 and it compare with the value
+*/
+//===
+console.log("2"===2)
+//here it check the datatype with the value .

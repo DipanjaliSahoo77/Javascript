8ece48922304f5023e7d18a9c58471b554dc487c
DIPANJALI SAHOO <139108012+DipanjaliSahoo77@users.noreply.github.com>
2026-01-17 07:55:31 +0000

    stack and heap memory in js

--- 01_basics/datatypessummary.js
+++ 01_basics/datatypessummary.js
@@ -21,3 +21,24 @@ const myFunction=function(){
 }
 //return of function is objectfunction
 console.log(typeof heros)//return object
+
+//*************Stack and Heap Memory*****//
+/*
+stack(primitive)=it gives a copy
+heap(non primitive)=it gives reference
+*/
+let myName="dipanjali"
+let anothername=myName
+anotherName="sinu"
+console.log(myName)
+console.log(anotherName)//it creates a copy and then it changes the value
+
+let user={
+    email:"dipa@gmail.com",
+    upi:"user@ybl"
+}//it goes to heap memory and it does not gives the copy , it gives the reference 
+let userTwo=user
+userTwo.email="dipa77@gmail.com"
+console.log(user.email)
+console.log(userTwo.email)//userTwo and user refer to one heap memory and both will  give the same result means whatever we are doing changes that happen in the original memory.
+

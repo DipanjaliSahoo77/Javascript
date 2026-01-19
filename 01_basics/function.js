397883dd1eb6d0ca38235b71a1c3fbd6dcef24e5
DIPANJALI SAHOO <139108012+DipanjaliSahoo77@users.noreply.github.com>
2026-01-19 05:57:18 +0000

    object and array in function in js

--- 01_basics/function.js
+++ 01_basics/function.js
@@ -26,3 +26,37 @@ function loginUserMessage(username  = "dipa"){
 }
 console.log(loginUserMessage());//here by deafult dipa will take place if we dont provide any username
 console.log(loginUserMessage("dipanjali"))//here dipanjali will take place
+
+//shopping card example
+function calculateCratPrice(...num1){
+return num1
+}
+console.log(calculateCratPrice(200,400,500))
+//rest operatior (...)or spread operator
+
+function calculatePrice(val1,val2,...num1){
+    return num1
+}
+
+console.log(calculatePrice(200,400,600,500,600))//here 200 and 400 will be assigned to val1 and val2 and rest value will be assigned to num1 as array
+
+const user={
+    username:"dipanjali",
+    price:21
+}
+//how to pass the object in the function
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user);
+//or
+handleObject({
+    username:"dipa",
+    price:34
+})
+//how to pass array in function
+const myNewArray=[200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]  
+}
+console.log(returnSecondValue(myNewArray))

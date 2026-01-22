e5ee7f35c3a75b435b346365f4347314a6916936
DIPANJALI SAHOO <139108012+DipanjaliSahoo77@users.noreply.github.com>
2026-01-22 00:47:46 +0000

    events in js

---
+++ 01_basics/events.js
@@ -0,0 +1,5 @@
+//javascript is a sequentially run language
+//event happens in the activity like mouse hover,drag and drop  and etc
+//event propagation is two types - event bubling (false,outside to inside)and event capturing (inside to outside)
+//e.stopPropagation()
+//e.preventDefault()
